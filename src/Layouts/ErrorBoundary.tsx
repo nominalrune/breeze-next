4c2bebdfe140be1f4acdf6aa03e66accfaa3a2e0
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2023-06-18 19:13:32 +0900

    improvement on ErrorBOundary

--- src/Layouts/ErrorBoundary.tsx
+++ src/Layouts/ErrorBoundary.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
-export class ErrorBoundary extends React.Component {
-	state={ hasError: false, error:undefined, errorInfo:undefined };
-    constructor(props: any) {
-        super(props);
-    }
-    static getDerivedStateFromError(error: any) {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
-    }
-    componentDidCatch(error: any, errorInfo: any) {
-        // You can also log the error to an error reporting service
-        this.setState({error,errorInfo})
-    }
-    render() {
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return<div className='fixed rounded-md border-red-800 bg-red-300'><h1>Error</h1><p>{this.state.error}</p><p>{this.state.errorInfo}</p></div>;
-        }
-        return this.props.children;
-    }
+import { FiAlertOctagon } from 'react-icons/fi';
+
+interface ErrorState {
+	hasError: boolean;
+	error: any;
+	errorInfo: any;
+}
+
+
+export class ErrorBoundary extends React.Component<{ children: React.ReactNode }, ErrorState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			hasError: false,
+			error: undefined,
+			errorInfo: undefined
+		};
+	}
+	static getDerivedStateFromError(error: any) {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true };
+	}
+	componentDidCatch(error: any, errorInfo: any) {
+		// You can also log the error to an error reporting service
+		this.setState({ error, errorInfo })
+		console.info({ error, errorInfo })
+	}
+	render() {
+		if (this.state.hasError) {
+			return <div className='grid justify-center w-full'>
+				<div className='bg-red-300/20 rounded-md m-3 p-5 '>
+					<h1 className='text-2xl flex gap-2 items-center'><FiAlertOctagon className='text-red-600' />エラーが発生しました</h1>
+					<div className="bg-red-50 rounded-xl m-6 p-3 px-6">
+						<p className='text-lg'>{this.state.error?.message}</p>
+						<p className='text-sm bg-white text-slate-600 max-h-40 overflow-y-scroll'>{this.state.error?.stack}</p>
+					</div>
+				</div>
+			</div>;
+		} else {
+			return this.props.children;
+		}
+	}
 }
