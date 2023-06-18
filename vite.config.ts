acc7322f67dcd21664b5ba37f8a3434231849d54
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2023-06-18 19:12:54 +0900

    ssl

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,49 @@ import * as path from "path";
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupReplace from "@rollup/plugin-replace";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import fs from "fs";
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-    root:"src",
-    plugins: [
-
-        tsconfigPaths(),
-        react(),
-        rollupReplace({
-            preventAssignment: true,
-            values: {
-                __DEV__: JSON.stringify(true),
-                "process.env.NODE_ENV": JSON.stringify("development"),
-            },
-        }),
-    ],
-    resolve: process.env.USE_SOURCE
-        ? {
-            alias: {
-                "@remix-run/router": path.resolve(
-                    __dirname,
-                    "../../packages/router/index.ts"
-                ),
-                "react-router": path.resolve(
-                    __dirname,
-                    "../../packages/react-router/index.ts"
-                ),
-                "react-router-dom": path.resolve(
-                    __dirname,
-                    "../../packages/react-router-dom/index.tsx"
-                ),
-            },
-        }
-        : {},
-        define: {
-            process:{env},
-        },
-}});
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		root: "src",
+		// server: {
+		// 	https: {
+		// 		key: fs.readFileSync('./certs/localhost-key.pem'),
+		// 		cert: fs.readFileSync('./certs/localhost.pem'),
+		// 	}
+		// },
+		plugins: [
+			tsconfigPaths(),
+			react(),
+			rollupReplace({
+				preventAssignment: true,
+				values: {
+					__DEV__: JSON.stringify(true),
+					"process.env.NODE_ENV": JSON.stringify("development"),
+				},
+			}),
+		],
+		resolve: process.env.USE_SOURCE
+			? {
+				alias: {
+					"@remix-run/router": path.resolve(
+						__dirname,
+						"../../packages/router/index.ts"
+					),
+					"react-router": path.resolve(
+						__dirname,
+						"../../packages/react-router/index.ts"
+					),
+					"react-router-dom": path.resolve(
+						__dirname,
+						"../../packages/react-router-dom/index.tsx"
+					),
+				},
+			}
+			: {},
+		define: {
+			process: { env },
+		},
+	};
+});
